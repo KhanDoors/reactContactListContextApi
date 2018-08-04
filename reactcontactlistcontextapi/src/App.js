4d9c3c96e3efd0650ad630b68f7f176b7ba4a375
unknown <syyadkhan@yahoo.com>
2018-08-04 12:16:15 -0400

    aug41216

--- reactcontactlistcontextapi/src/App.js
+++ reactcontactlistcontextapi/src/App.js
@@ -1,13 +1,16 @@
-import React, {
-  Component
-} from 'react';
-
+import React, { Component } from 'react';
+import Contact from './components/Contact';
+import Header from './components/Header';
 import './App.css';
 
 class App extends Component {
   render() {
-    return ( <div className = "App">
-      <h1> The Roof is on Fire! </h1>  </div>
+    return ( 
+    <div className = "App">
+    < Header branding="Contact Manager" />
+    < Contact name="John Doe" phone="555-555-5555" email="jdoe@gmail.com" />
+    < Contact name="Jane Doe" phone="555-555-6666" email="janedoe@gmail.com" />
+    </div>
     );
   }
 }
