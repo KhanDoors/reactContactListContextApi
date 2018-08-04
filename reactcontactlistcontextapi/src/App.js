ff9dfa3dc54ff99e3598f705812e6c1d2228e0e9
unknown <syyadkhan@yahoo.com>
2018-08-04 12:58:34 -0400

    bootstrap

--- reactcontactlistcontextapi/src/App.js
+++ reactcontactlistcontextapi/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Contact from './components/Contact';
 import Header from './components/Header';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 class App extends Component {
@@ -8,8 +9,10 @@ class App extends Component {
     return ( 
     <div className = "App">
     < Header branding="Contact Manager" />
+    <div className="container">
     < Contact name="John Doe" phone="555-555-5555" email="jdoe@gmail.com" />
     < Contact name="Jane Doe" phone="555-555-6666" email="janedoe@gmail.com" />
+    </div>    
     </div>
     );
   }
