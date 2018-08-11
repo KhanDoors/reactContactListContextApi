5cd67a5c3bc1ed5159325930fc8226f62964f819
unknown <syyadkhan@yahoo.com>
2018-08-11 02:08:18 -0400

    aug110208

--- reactcontactlistcontextapi/src/components/Contact.js
+++ reactcontactlistcontextapi/src/components/Contact.js
@@ -1,39 +1,50 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
+class Contact extends Component {
+  state = {
+    showContactInfo: false
+  };
 
- class Contact extends Component {
-    state = {
-        showContactInfo: false
-    };
-
-    onDeleteClick = () => {
-        this.props.deleteClickHandler();
-    }
+  onDeleteClick = () => {
+    this.props.deleteClickHandler();
+  };
 
   render() {
     const { name, email, phone } = this.props.contact;
     const { showContactInfo } = this.state;
 
     return (
-        <div className="card card-body mb-3">
-        <h4>{name} <i onClick={() => this.setState({ showContactInfo: !this.state.showContactInfo})} className="fas fa-sort-down" style={{ cursor: 'pointer' }} />
-        <i className="fa fa-times" style={{ cursor: 'pointer', float: 'right', color: 'red'}}
-        onClick={this.onDeleteClick} />
+      <div className="card card-body mb-3">
+        <h4>
+          {name}{" "}
+          <i
+            onClick={() =>
+              this.setState({ showContactInfo: !this.state.showContactInfo })
+            }
+            className="fas fa-sort-down"
+            style={{ cursor: "pointer" }}
+          />
+          <i
+            className="fa fa-times"
+            style={{ cursor: "pointer", float: "right", color: "red" }}
+            onClick={this.onDeleteClick}
+          />
         </h4>
-        {showContactInfo ? (<ul className="list-group">
+        {showContactInfo ? (
+          <ul className="list-group">
             <li className="list-group-item">Email: {email} </li>
             <li className="list-group-item">Phone: {phone}</li>
-        </ul>) : null}
-        
+          </ul>
+        ) : null}
       </div>
-    )
+    );
   }
 }
 
 Contact.PropTypes = {
-    contact: PropTypes.object.isRequired,
-    deleteClickHandler: PropTypes.func.isRequired
-}
+  contact: PropTypes.object.isRequired,
+  deleteClickHandler: PropTypes.func.isRequired
+};
 
 export default Contact;
