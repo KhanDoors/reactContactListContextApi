dce1d223c8c0b7ca67b71e2e113b5f2ac0214df1
unknown <syyadkhan@yahoo.com>
2018-08-11 20:10:26 -0400

    validationin

--- reactcontactlistcontextapi/src/components/contacts/AddContact.js
+++ reactcontactlistcontextapi/src/components/contacts/AddContact.js
@@ -7,13 +7,29 @@ class AddContact extends Component {
   state = {
     name: "",
     email: "",
-    phone: ""
+    phone: "",
+    errors: {}
   };
 
   onSubmit = (dispatch, e) => {
     e.preventDefault();
 
     const { name, email, phone } = this.state;
+
+    if (name === "") {
+      this.setState({ errors: { name: "Name is Required" } });
+      return;
+    }
+
+    if (email === "") {
+      this.setState({ errors: { email: "Email is Required" } });
+      return;
+    }
+
+    if (phone === "") {
+      this.setState({ errors: { phone: "Phone is Required" } });
+      return;
+    }
 
     const newContact = {
       id: uuid(),
@@ -27,14 +43,15 @@ class AddContact extends Component {
     this.setState({
       name: "",
       email: "",
-      phone: ""
+      phone: "",
+      errors: {}
     });
   };
 
   onChange = e => this.setState({ [e.target.name]: e.target.value });
 
   render() {
-    const { name, email, phone } = this.state;
+    const { name, email, phone, errors } = this.state;
 
     return (
       <Consumer>
@@ -51,14 +68,16 @@ class AddContact extends Component {
                     placeholder="Enter Name"
                     value={name}
                     onChange={this.onChange}
+                    error={errors.name}
                   />
                   <TextInputGroup
                     label="Email"
                     name="email"
                     type="email"
-                    placeholder="Enter Email Address"
+                    placeholder="Enter Email"
                     value={email}
                     onChange={this.onChange}
+                    error={errors.email}
                   />
                   <TextInputGroup
                     label="Phone"
@@ -66,6 +85,7 @@ class AddContact extends Component {
                     placeholder="Enter Phone #"
                     value={phone}
                     onChange={this.onChange}
+                    error={errors.phone}
                   />
                   <input
                     type="submit"
